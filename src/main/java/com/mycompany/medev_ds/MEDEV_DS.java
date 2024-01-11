e1924059202bc4f5a1c6701b42729af8674b05c3
Tomle53 <tom.leonard@eleves.ec-nantes.fr>
2024-01-11 08:24:08 +0100

    init project

---
+++ src/main/java/com/mycompany/medev_ds/MEDEV_DS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.medev_ds;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class MEDEV_DS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
